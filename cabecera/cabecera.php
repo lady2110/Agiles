4e69ccada13b5dd932ac552704e53b619a64ac56
Leidy <lady-2110@hotmail.com>
2021-10-05 19:22:28 -0500

    cambio del color de fondo y banner de pie de pagina

--- cabecera/cabecera.php
+++ cabecera/cabecera.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,40 +9,40 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <style>
-        @import url('https://fonts.googleapis.com/css2?family=Gluten:wght@300&display=swap');    
-    a{
-        font-family: 'Gluten', cursive;
-        
-    }
+        @import url('https://fonts.googleapis.com/css2?family=Gluten:wght@300&display=swap');
+
+        a {
+            font-family: 'Gluten', cursive;
+
+        }
     </style>
 </head>
-<body background="images/fondo1.jpg">
-    
-        <div id="banner">
-            <img src="images/Pasteleria.png" style="width:100%; height: 250px">
-        </div>
-        <div class="BarraNavegacion">
+
+<body style="background-color: #FCE4EC;">
+
+    <div id="banner">
+        <img src="images/Pasteleria.png" style="width:100%; height: 250px">
+    </div>
+    <div class="BarraNavegacion">
         <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #F48FB1;">
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto" style="margin-left: 25%;">
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.php"><img src="images/home.svg" width="30px" height="30px"> Home |<span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="historia.php"><img src="images/nosotros.svg" width="30px" height="30px"> Nuestra empresa |</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="carrito.php"><img src="images/carrito.svg" width="30px" height="30px"> Carrito |</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="registros.php"><img src="images/empleados.svg" width="30px" height="30px"> Inicie Sección</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto" style="margin-left: 30%;">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php"><img src="images/home.svg" width="30px" height="30px"> Home |<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="historia.php"><img src="images/nosotros.svg" width="30px" height="30px"> Nuestra empresa |</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="carrito.php"><img src="images/carrito.svg" width="30px" height="30px"> Carrito |</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="registros.php"><img src="images/empleados.svg" width="30px" height="30px"> Inicie Sección</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
--- cabecera/pie.php
+++ cabecera/pie.php
@@ -2,52 +2,52 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <style>
-     
-    .informacion{
-        margin-top: 10px;
-        width: 100%;
-        height: 400px;
-        background-image:url("images/fondo.png");
-    }
-    .redes, .contacto, .horario{
-        width: 24%;;
-        height: 300px;
-        float: left;
-        margin-top: 50px;
-        margin-left: 7%;
-        font-family: 'Gluten', cursive;
-        color: white;
-        text-align: center;
-    }
+        .informacion {
+            margin-top: 10px;
+            width: 100%;
+            height: 400px;
+            background-image: url("images/pie.png");
+        }
+
+        .redes,
+        .contacto,
+        .horario {
+            width: 24%;
+            ;
+            height: 300px;
+            float: left;
+            margin-top: 50px;
+            margin-left: 7%;
+            font-family: 'Gluten', cursive;
+            color: #EC407A;
+            text-align: center;
+        }
     </style>
-<div class="informacion">
-            <div class="redes">
-                <h2>Redes Sociales</h2>
-                <img src="images/facebook.png" style="width: 50px; height:50px"><h2>FACEBOOK</h2>
-                <img src="images/instagram.png" style="width: 50px; height:50px"><h2>INSTRAGRAM</h2>
-            </div>
-            <div class="contacto">
-                <h2>Ubicación</h2>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1402.092171473039!2d-75.56953142432347!3d6.301060003878885!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e442f2604a9a52f%3A0xd27da03066a27ccd!2sCl.%20104%20%2368a20%2C%20Medell%C3%ADn%2C%20Antioquia!5e0!3m2!1ses!2sco!4v1632795133583!5m2!1ses!2sco" width="300" height="250" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-            </div>
-                <div class="horario">
-                <h2>Contáctenos</h2>
-                <h4>Lun - Vie: 7:00 am - 7:00 pm
+    <div class="informacion">
+        <div class="redes">
+            <h2>Redes Sociales</h2>
+            <img src="images/facebook.png" style="width: 50px; height:50px">
+            <h2>FACEBOOK</h2>
+            <img src="images/instagram.png" style="width: 50px; height:50px">
+            <h2>INSTRAGRAM</h2>
+        </div>
+        <div class="contacto">
+            <h2>Ubicación</h2>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1402.092171473039!2d-75.56953142432347!3d6.301060003878885!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e442f2604a9a52f%3A0xd27da03066a27ccd!2sCl.%20104%20%2368a20%2C%20Medell%C3%ADn%2C%20Antioquia!5e0!3m2!1ses!2sco!4v1632795133583!5m2!1ses!2sco" width="300" height="250" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+        <div class="horario">
+            <h2>Contáctenos</h2>
+            <h4>Lun - Vie: 7:00 am - 7:00 pm
                 Sabados: 7:00 am - 5:00 pm</h4>
             <h2>Domicilio</h2>
-                <img src="images/repartidor.png" style="width: 50px; height:50px">
-                <h4>3122278893</h4>
-                <h4>3122269457</h4>
-            </div>
+            <img src="images/repartidor.png" style="width: 50px; height:50px">
+            <h4>3122278893</h4>
+            <h4>3122269457</h4>
         </div>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-            crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-            integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
-            crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
-            integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s"
-            crossorigin="anonymous"></script>
-</body>
-</html>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    </body>
+
+    </html>
